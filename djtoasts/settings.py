f773d4ad6933a9f813856c8fc14ad7238b3f29d8
Josh Karamuth <hello@joshkaramuth.com>
2025-01-01 15:02:51 +0400

    Use middleware instead of fetching toasts after every request

--- djtoasts/settings.py
+++ djtoasts/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "toaster.middleware.toaster_middleware",
 ]
 
 ROOT_URLCONF = "djtoasts.urls"
